96767a87869e989f14966134c6f5e565435a47f0
awaludin-dev <ubedsf@gmail.com>
2022-03-08 15:53:16 +0800

    delete vanta and glassmorphysm

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 // import React, { useState, useEffect, useRef } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// import BIRDS from 'vanta/dist/vanta.birds.min';
-// import { CustomCard } from '@tsamantanis/react-glassmorphism'
-import '@tsamantanis/react-glassmorphism/dist/index.css'
 import './App.css';
 import Footer from './Component/Footer';
 // import Biodata from './Pages/Biodata';
 import Home from './Pages/Home';
  
 const App = () => {
-  // const [vantaEffect, setVantaEffect] = useState(0)
-  // const myRef = useRef(null)
-  // useEffect(() => {
-  //   if (!vantaEffect) {
-  //     setVantaEffect(BIRDS({
-  //       el: myRef.current,
-  //       mouseControls: true,
-  //       touchControls: true,
-  //       gyroControls: false,
-  //       minHeight: 200.00,
-  //       minWidth: 200.00,
-  //       scale: 1.00,
-  //       scaleMobile: 1.00,
-  //       birdSize: 0.80
-  //     }))
-  //   }
-  //   return () => {
-  //     if (vantaEffect) vantaEffect.destroy()
-  //   }
-  // }, [vantaEffect])
   return(
     <div>
       <BrowserRouter>
